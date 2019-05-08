7ac30a2828a6879ebf694c17458102f587eb02fc
bharath2232 <33877113+bharath2232@users.noreply.github.com>
2019-05-08 21:51:02 +0200

    Fixed Minor bugs

--- react-otp/src/components/SendOtp.js
+++ react-otp/src/components/SendOtp.js
@@ -1,121 +1,139 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import idGenerator from 'react-id-generator';
-
+import OTP from 'otp-client';
 
 
 const styles = {
-  card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
 };
 
 class SendOtp extends Component {
 
-  state = {
-    phoneNo: '',
-    age: '',
-    multiline: 'Controlled',
-    enterOtp: '',
-    otp:idGenerator("REACTOTP"),
-    verified: ''
-  };
+    state = {
+        phoneNo: '',
+        age: '',
+        multiline: 'Controlled',
+        enterOtp: '',
+        otp: idGenerator("REACTOTP"),
+        verified: ''
+    };
 
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
+    componentDidMount() {
+        const secret = "TPQDAHVBZ5NBO5LFEQKC7V7UPATSSMFY"
+        const options = {
+            algorithm: "sha256",
+            digits: 8,
+            period: 20
+        }
 
-sendOtpHandler = () => {
-console.log('logged',this.state.name)
+        const otp = new OTP(secret, options)
+        const token = otp.getToken()
+        this.setState({otp:token})
+        console.log('working',this.state.otp);
+    }
 
-console.log('otp',this.state.otp);
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
 
-const params = {
-  apikey : 'P+uEzfbqb0I-5LZubEfVBlAa41jcKaE9N7LbHUUM71',
-  numbers: '91'+this.state.phoneNo,
-  message:'Your OTP is ' + this.state.otp
-  }
-axios.get('https://api.textlocal.in/send/',{params:params})
-  .then( (response)=> {
-    console.log(response);
-  })
-  .catch( (error) => {
-    console.log(error);
-  });
-}
-verifiyOtpHandler=()=>{
-if(this.state.enterOtp === this.state.otp){
-  return this.setState({verified:true});
-}
-if(this.state.enterOtp !== this.state.otp){
-  return this.setState({verified:false});
-}
-}
+    sendOtpHandler = () => {
+        console.log('logged', this.state.name)
 
+        console.log('otp', this.state.otp);
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div style={{margin:250}}>
-      <TextField
-          id="outlined-name"
-          label="Phone Number"
-          value={this.state.phoneNo}
-          onChange={this.handleChange('phoneNo')}
-          margin="normal"
-          variant="outlined"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">+91</InputAdornment>,
-          }}
-        />  
-
-        <Button style={{margin: 10,marginTop:25}}  variant="contained" onClick={this.sendOtpHandler} color="primary">
-       Send otp        
-      </Button>
-
-      <TextField
-          id="outlined-name"
-          label="ENTER OTP"
-          value={this.state.enterOtp}
-          onChange={this.handleChange('enterOtp')}
-          margin="normal"
-          variant="outlined"
-        />  
-        <Button style={{margin: 10,marginTop:25}} variant="contained" onClick={this.verifiyOtpHandler} color="primary">
-          Verifiy
-      </Button>
-      <div>
-         <Card className={classes.card}>   
-      {this.state.verified === true ? <Typography>OTP Verified </Typography>:<Typography></Typography>}
-      {this.state.verified === false ? <Typography>Invalid Otp</Typography>:<Typography></Typography>}
+        const params = {
+            apikey: 'P+uEzfbqb0I-5LZubEfVBlAa41jcKaE9N7LbHUUM71',
+            numbers: '91' + this.state.phoneNo,
+            message: 'Your OTP is ' + this.state.otp
+        }
+        axios.get('https://api.textlocal.in/send/', {params: params})
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+    verifiyOtpHandler = () => {
+        if (this.state.enterOtp === this.state.otp) {
+            return this.setState({verified: true});
+        }
+        if (this.state.enterOtp !== this.state.otp) {
+            return this.setState({verified: false});
+        }
+    }
 
 
-    </Card>
-    <Typography>Only Indian Number</Typography>
-      </div>
-      </div>
-     
-    );
-  }
+    render() {
+        const {classes} = this.props;
+        return (
+            <div style={{margin: 250}}>
+                <TextField
+                    id="outlined-name"
+                    label="Phone Number"
+                    value={this.state.phoneNo}
+                    onChange={this.handleChange('phoneNo')}
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">+91</InputAdornment>,
+                    }}
+                />
+
+                <Button style={{margin: 10, marginTop: 25}} variant="contained" onClick={this.sendOtpHandler}
+                        color="primary">
+                    Send otp
+                </Button>
+
+                <TextField
+                    id="outlined-name"
+                    label="ENTER OTP"
+                    value={this.state.enterOtp}
+                    onChange={this.handleChange('enterOtp')}
+                    margin="normal"
+                    variant="outlined"
+                />
+                <Button style={{margin: 10, marginTop: 25}} variant="contained" onClick={this.verifiyOtpHandler}
+                        color="primary">
+                    Verifiy
+                </Button>
+                <div>
+                    <Card className={classes.card}>
+                        {this.state.verified === true ? <Typography>OTP Verified </Typography> :
+                            <Typography></Typography>}
+                        {this.state.verified === false ? <Typography>Invalid Otp</Typography> :
+                            <Typography></Typography>}
+
+
+                    </Card>
+                    <Typography>Only Indian Number, Check console log for Json response from SMS operater</Typography>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default withStyles(styles)(SendOtp);
